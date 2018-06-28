6be575d4a33273da2909bc217c15e48ea5097c67
Williams Chidi <williamschidi1@gmail.com>
2018-06-28 16:09:52 +0100

    Added mongoose and mongoose config

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const debug = require('debug')('express-mongo-jwt:app');
 const cors = require('cors');
 const indexRouter = require('./api/routes/index');
 const usersRouter = require('./api/routes/users');
+const config = require('./config');
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+
+// MongoDB Connection
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(
+    config.mongoURL,
+    (err) => {
+      if (err) {
+        debug('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+        throw err;
+      }
+    }
+  );
+}
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
