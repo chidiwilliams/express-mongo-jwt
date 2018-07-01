7417e8d8f16d7c757aaf88eaa5f475f3fbcc4eca
Williams Chidi <williamschidi1@gmail.com>
2018-07-01 19:38:11 +0100

    Stopped testing for now. Will set up when I get data.

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const UserController = require('../controllers/user.controller');
-const router = express.Router();
-
 const Post = require('../models/post');
 
-router.get('/users', UserController.index);
+const router = express.Router();
 
-router.get('/blobs', (req, res) => {
-  return Post.find()
-    .exec()
-    .then((posts) => {
-      return res.status(200).json({
-        hell: 'wfewfe',
-      });
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        not: 'Working'
-      })
-    });
-});
+router.get('/users', UserController.index);
 
 module.exports = router;
